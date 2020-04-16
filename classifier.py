16f9d2ec5bf009e7cfee7542995442a2bcf896e6
Ishpreet <ishpreetsingh2808@gmail.com>
2020-04-16 18:11:53 -0400

    Linear Classifier Implemented

---
+++ classifier.py
@@ -0,0 +1,129 @@
+
+import numpy as np
+import pandas as pd
+import csv
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from sklearn import feature_extraction, model_selection, metrics, svm
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.svm import LinearSVC, SVC
+from sklearn.model_selection import train_test_split
+
+
+def read_dataset():
+    csv1_path = "dataset/icse_id.csv"
+    csv2_path = "dataset/vldb_id.csv"
+
+    df1 = pd.read_csv(csv1_path)
+    df2 = pd.read_csv(csv2_path)
+    df1.columns = ['Raw Text', 'Clean Text', 'Year', 'Date', 'Series',
+                  'Conference Type', 'Publication Type', 'Serial No1', 'Serial No2', 'Serial No3']
+    df2.columns = ['Index No', 'Raw Text', 'Clean Text', 'Year', 'Date', 'Series',
+                  'Conference Type', 'Publication Type', 'Serial No1', 'Serial No2', 'Serial No3']
+
+    return df1['Clean Text'].values, df1['Publication Type'].values, df2['Clean Text'].values, df2['Publication Type'].values
+
+
+def preprocess_data(data1, labels1, data2, labels2):
+    data1 = clean_dataset(data1)
+    data2 = clean_dataset(data2)
+    data = list(data1) + list(data2)
+    labels = list(labels1) + list(labels2)
+    
+    training_set = np.column_stack((data, labels))
+    np.random.shuffle(training_set)
+    data = training_set[:,:1]
+    labels = training_set[:,1:]
+    data = [row[0] for row in data]
+    labels = [row[0] for row in labels]
+    labels = [1 if y == 'icse' else 0 for y in labels]
+
+    return data, labels
+
+
+def clean_dataset(X):
+
+    cachedStopWords = stopwords.words("english")
+
+    for idx in range(X.shape[0]):
+        X[idx] = ' '.join([word for word in X[idx].split() if word not in cachedStopWords])
+    return X
+
+
+def corpus_dictionary(corpus):
+    words = []
+    for line in corpus:
+        words.extend(line.split())
+    dic = Counter(words)
+    dic = dict(dic.most_common(3000))
+    return dic
+
+
+def feature_extraction(X):
+
+    dic = corpus_dictionary(X)
+    words = list(dic.keys())
+    feature_matrix = np.zeros((len(X), len(words)))
+    
+    for row in range(len(X)):
+        sentence = X[row]
+        words_in_sentence = sentence.split()
+        
+        for col in range(len(words)):
+            word = words[col]
+            
+            if word in words_in_sentence:
+                feature_matrix[row,col] = 1
+
+    return feature_matrix
+
+
+def get_results(y_test, y_predict): 
+    
+    conf_matrix = confusion_matrix(y_test, y_predict)
+    print("\nConfuction Rate: \n", conf_matrix)
+
+    f_score = f1_score(y_test, y_predict, average="macro")
+    print("F Score: ", f_score)
+    
+    precision = precision_score(y_test, y_predict, average="macro")
+    print("Precision: ", precision)
+    
+    recall = recall_score(y_test, y_predict, average="macro")
+    print("Recall: ", recall)
+
+    ham_misclassification_rate = float(conf_matrix[0][1]/(conf_matrix[0][0] + conf_matrix[0][1]))
+    print("Ham Miscalssification Rate: ", str(round(ham_misclassification_rate, 4)))
+
+    spam_misclassification_rate = float(conf_matrix[1][0]/(conf_matrix[1][0] + conf_matrix[1][1]))
+    print("Spam Miscalssification Rate: ", str(round(spam_misclassification_rate, 4)))
+
+
+def linear_model():
+    return LinearSVC()
+
+
+def polynomial_model():
+    return SVC(kernel='poly', degree=4)
+
+
+if __name__ == "__main__":
+
+    data1, labels1, data2, labels2 = read_dataset()
+    
+    data, labels = preprocess_data(data1, labels1, data2, labels2)
+
+    feature_matrix = feature_extraction(data)
+
+    x_train, x_test, y_train, y_test = train_test_split(feature_matrix, labels, test_size=0.40)
+
+    model = linear_model()
+    
+    model.fit(x_train, y_train)
+
+    y_predict = model.predict(x_test)
+    
+    get_results(y_test, y_predict)
+    
+    
